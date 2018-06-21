c8ec455cb41d74a2215a9f8cc3d5f968f3d75f13
Adi <gitkraken@twocowmail.net>
2018-06-21 20:22:06 +0530

    final upload

--- js/script.js
+++ js/script.js
@@ -1,59 +1,49 @@
-window.onload = function () {
-    arrowscroll()
+window.onload = function() {
+	arrowscroll()
 };
-window.onscroll = function () {
-    scroll(), arrowscroll()
+window.onscroll = function() {
+	scroll(), arrowscroll()
 };
-
-
 
 function scroll() {
-    if (document.documentElement.scrollTop > 15) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
-        document.getElementById("navbar").style.top = "-80px";
-    }
+	if (document.documentElement.scrollTop > 15) {
+		document.getElementById("navbar").style.top = "0";
+	} else {
+		document.getElementById("navbar").style.top = "-80px";
+	}
 }
-
-
-$("#about_nav").click(function () {
-    $('html, body').animate({
-        scrollTop: $("#about").offset().top
-    }, 1000);
+$("#about_nav").click(function() {
+	$('html, body').animate({
+		scrollTop: $("#about").offset().top
+	}, 1000);
 });
-
-$("#contact_nav").click(function () {
-    $('html, body').animate({
-        scrollTop: $("#contact").offset().top
-    }, 1000);
+$("#contact_nav").click(function() {
+	$('html, body').animate({
+		scrollTop: $("#contact").offset().top
+	}, 1000);
 });
-
-$("#project").click(function () {
-    $('html, body').animate({
-        scrollTop: $("#projects").offset().top
-    }, 1000);
+$("#project").click(function() {
+	$('html, body').animate({
+		scrollTop: $("#projects").offset().top
+	}, 1000);
 });
-
-$("#home").click(function () {
-    $('html, body').animate({
-        scrollTop: $("#top").offset().top
-    }, 1000);
+$("#home").click(function() {
+	$('html, body').animate({
+		scrollTop: $("#top").offset().top
+	}, 1000);
 });
-
-$("#arrow").click(function () {
-    $('html, body').animate({
-        scrollTop: $("#top").offset().top
-    }, 1000);
+$("#arrow").click(function() {
+	$('html, body').animate({
+		scrollTop: $("#top").offset().top
+	}, 1000);
 });
-
-$("#logo").click(function () {
-    $('html, body').animate({
-        scrollTop: $("#top").offset().top
-    }, 1000);
+$("#logo").click(function() {
+	$('html, body').animate({
+		scrollTop: $("#top").offset().top
+	}, 1000);
 });
-
-$("#know_more_button").click(function () {
-    $('html, body').animate({
-        scrollTop: $("#about").offset().top
-    }, 1000);
+$("#know_more_button").click(function() {
+	$('html, body').animate({
+		scrollTop: $("#about").offset().top
+	}, 1000);
 });
